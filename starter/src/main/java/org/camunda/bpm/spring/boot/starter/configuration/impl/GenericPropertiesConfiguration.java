7195ab502c7573d444e388c2969d3dca7e67b6b3
Yana Vasileva <yana.vasileva@camunda.com>
2018-03-01 16:59:35 +0100

    changes for spring-boot 2.0.0

--- starter/src/main/java/org/camunda/bpm/spring/boot/starter/configuration/impl/GenericPropertiesConfiguration.java
+++ starter/src/main/java/org/camunda/bpm/spring/boot/starter/configuration/impl/GenericPropertiesConfiguration.java
@@ -7,22 +7,30 @@ import org.camunda.bpm.spring.boot.starter.property.GenericProperties;
 import org.camunda.bpm.spring.boot.starter.configuration.Ordering;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.PropertyValues;
-import org.springframework.boot.bind.RelaxedDataBinder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.util.CollectionUtils;
 
 @Order(Ordering.DEFAULT_ORDER - 1)
 public class GenericPropertiesConfiguration extends AbstractCamundaConfiguration {
 
+  @Autowired
+public ApplicationContext applicationContext;
+
   @Override
   public void preInit(SpringProcessEngineConfiguration springProcessEngineConfiguration) {
     GenericProperties genericProperties = camundaBpmProperties.getGenericProperties();
     final Map<String, Object> properties = genericProperties.getProperties();
     if (!CollectionUtils.isEmpty(properties)) {
-      RelaxedDataBinder relaxedDataBinder = new RelaxedDataBinder(springProcessEngineConfiguration);
-      relaxedDataBinder.setIgnoreInvalidFields(genericProperties.isIgnoreInvalidFields());
-      relaxedDataBinder.setIgnoreUnknownFields(genericProperties.isIgnoreUnknownFields());
-      relaxedDataBinder.bind(getPropertyValues(properties));
+      // RelaxedDataBinder relaxedDataBinder = new RelaxedDataBinder(springProcessEngineConfiguration);
+      // relaxedDataBinder.setIgnoreInvalidFields(genericProperties.isIgnoreInvalidFields());
+      // relaxedDataBinder.setIgnoreUnknownFields(genericProperties.isIgnoreUnknownFields());
+      // relaxedDataBinder.bind(getPropertyValues(properties));
+      Binder binder = Binder.get(applicationContext.getEnvironment());
+      binder.bind("GenericProperties", Bindable.of(GenericProperties.class));
       logger.debug("properties bound to configuration: {}", genericProperties);
     }
   }
